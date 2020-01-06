3bc104d38c32c9276a3f8a4f1eee54ad88b9bd65
Sylvain Loiseau <sloiseau@MacBook-Air-de-Sylvain.local>
2020-01-06 15:35:48 +0100

    clean

--- R/FifSampling.R
+++ R/FifSampling.R
@@ -28,57 +28,92 @@ counterbalanced.sampling <- function (data, prop1, prop2, sample.nb, maxrepetiti
   if (!is.null(maxrepetition) & !is.numeric(maxrepetition))
     stop("'maxrepetition' must be a numeric vector");
 
-  prop1.nb <- length(unique(data[[prop1]]))
-  prop2.nb <- length(unique(data[[prop2]]))
-  items.nb <- prop1.nb * prop2.nb
-  sample.size <- items.nb / sample.nb
-
   cat("Sampling\n")
   # Assign each item to a sample with a balanced number of prop1 and prop2 in each sample
-  samples.index <- counterbalanced.sample.index.matrix(data, prop1, prop2, prop1.nb, prop2.nb, sample.nb);
+  samples.index <- counterbalanced.sample.index.matrix(data, prop1, prop2, sample.nb);
 
   ls <- vector(mode = "list", length=sample.nb)
   for (i in 1:sample.nb) {
     ls[[i]] <- data[samples.index[i,],]
   }
-  #samples <- split(data, as.vector(t(samples.index)));
 
   # reorder in order to avoid repetition
+  reordered <- vector(mode="list", length=sample.nb);
   if (!is.null(maxrepetition)) {
     cat("Ordering\n")
-    reordered <- lapply(ls, function(x) { order.without.repetition(x, c(prop1, prop2), maxrepetition) });
-    # TODO ugly...
-    while (reordered[[1]][[1]][1] == FALSE | reordered[[2]][[1]][1] == FALSE | reordered[[3]][[1]][1] == FALSE) {
-      reordered <- lapply(ls, function(x) { order.without.repetition(x, c(prop1, prop2), maxrepetition)});
+    for (i in 1:sample.nb) {
+      reordered[[i]] <- order.without.repetition(ls[[i]], c(prop1, prop2), maxrepetition);
     }
     cat("\n")
+  } else {
+    reordered <- ls
   }
   cat("ok\n")
   return(reordered)
 }
 
-order.without.repetition <- function(sample.df, cols, maxrepetition) {
+#' Reorder the lines of a data frame so that there is no more
+#' that the given repetition of a modality in the given columns.
+#'
+#' @param data a data frame
+#'
+#' @param cols a character vector giving the name of the column
+#' of the data frame for which constraint on the repetition have to be observed.
+#'
+#' @param maxrepetition a numeric vector of the same length
+#' than 'cols' giving the the maximum consecutive identical modality allowed.
+#'
+#' @return the same data frame as 'data', with the lines reordered
+#'
+#' @export
+#'
+#' @examples
+#'   data <- data.frame(
+#'     A=c(rep("a", 2), rep("b", 2)),
+#'     B=c(rep("c", 2), rep("d", 2))
+#'   );
+#'   order.without.repetition(data, c("A", "B"), c(1,1));
+order.without.repetition <- function(data, cols, maxrepetition, maxtry=50) {
+  ntry <- 1;
+  res <- try.order.without.repetition(data, cols, maxrepetition);
+  while (is.logical(res)) {
+    cat(".")
+    res <- try.order.without.repetition(data, cols, maxrepetition);
+    ntry <- ntry + 1
+    if (ntry >= maxtry) {
+      cat("\n")
+      stop("the maximum number of tries has been reached")
+    }
+  }
+  return(res)
+}
+
+#' [Private]
+try.order.without.repetition <- function(data, cols, maxrepetition) {
    if (length(cols) != length(maxrepetition)) stop("'cols' and 'maxrepetition' length must match.")
    index.ordered.wo.repetition <- c();
-   sample.size <- nrow(sample.df)
+   sample.size <- nrow(data)
    remaining <- 1:sample.size
+
+   # the first element
    first.i <- sample(remaining, 1)
    index.ordered.wo.repetition[1] <- first.i;
    remaining <- remaining[-first.i];
+
+   # the remaining elements
    for (i in 2:sample.size) {
      possible <- remaining;
      for(j in 1:length(maxrepetition)) {
        col <- cols[j]
        maxrep <- maxrepetition[j]
        previous.i <- index.ordered.wo.repetition[max(1,i-maxrep):(i-1)];
-       previous.val <- sample.df[previous.i, col]
-       if (length(unique(previous.val) == maxrep)) {
-         possible <- intersect(which(sample.df[, col] != previous.val[1]), possible);
+       previous.val <- data[previous.i, col]
+       if (length(unique(previous.val) == 1)) {
+         possible <- intersect(which(data[, col] != previous.val[1]), possible);
        }
      }
      if(length(possible) == 0) {
-       cat(".")
-       return(list(data.frame(FALSE)));
+       return(FALSE);
      }
      found <- 0;
      if (length(possible) == 1) {
@@ -90,11 +125,11 @@ order.without.repetition <- function(sample.df, cols, maxrepetition) {
      remaining <- remaining[-which(remaining == found)];
    }
    if (length(index.ordered.wo.repetition) != sample.size) stop("The ordering index does not fit the length of the data.")
-   res <- sample.df[index.ordered.wo.repetition,]
+   res <- data[index.ordered.wo.repetition,]
    return(res)
 }
 
-
+#' [Private]
 #' Create a matrix of sample index without repetition of the index in a given row,
 #' and with a balanced repetition of index in a given column.
 #'
@@ -105,8 +140,10 @@ order.without.repetition <- function(sample.df, cols, maxrepetition) {
 #' @return a matrix of numerics
 #'
 #' @examples
-counterbalanced.sample.index.matrix <- function(data, prop1, prop2, prop1.nb, prop2.nb, sample.nb) {
+counterbalanced.sample.index.matrix <- function(data, prop1, prop2, sample.nb) {
 
+  prop1.nb <- length(unique(data[[prop1]]))
+  prop2.nb <- length(unique(data[[prop2]]))
   items.nb <- prop1.nb * prop2.nb
   sample.size <- items.nb / sample.nb;
 
@@ -118,13 +155,14 @@ counterbalanced.sample.index.matrix <- function(data, prop1, prop2, prop1.nb, pr
   if ((sample.size %% prop2.nb) != 0)
     stop("The sample size is not a multiple of the number of modality in prop2. The samples cannot be balanced.")
 
-    square <- FALSE
-    while (square[1]==FALSE) {
-      square <- fill.counterbalanced.sample.index.matrix(data, prop1, prop2, prop1.nb, prop2.nb, sample.nb)
+    newdata <- FALSE
+    while (newdata[1]==FALSE) {
+      newdata <- fill.counterbalanced.sample.index.matrix(data, prop1, prop2, prop1.nb, prop2.nb, sample.nb)
     }
-    return(square)
+    return(newdata)
   }
 
+#' [Private]
 #' Try to fill a matrix. If we ends up into a dead end, with no more
 #' logical solution, we give up and return FALSE
 #'
@@ -138,19 +176,19 @@ counterbalanced.sample.index.matrix <- function(data, prop1, prop2, prop1.nb, pr
 fill.counterbalanced.sample.index.matrix <- function(data, prop1, prop2, prop1.nb, prop2.nb, sample.nb) {
   items.nb <- prop1.nb * prop2.nb
   sample.size <- items.nb / sample.nb;
-  square <- matrix(0, nrow=sample.nb, ncol=sample.size);
+  newdata <- matrix(0, nrow=sample.nb, ncol=sample.size);
 
   remaining <- 1:items.nb;
 
   for (row in 1:sample.nb) {
     first.i <- sample(remaining, 1)
-    square[row, 1] <- first.i;
+    newdata[row, 1] <- first.i;
     remaining <- remaining[-first.i];
     for (column in 2:sample.size) {
       # TODO ugly...
-      prop1.max.occ.reached.t <- table(data[square[row,1:(column-1)], prop1]) == (sample.size / prop1.nb)
+      prop1.max.occ.reached.t <- table(data[newdata[row,1:(column-1)], prop1]) == (sample.size / prop1.nb)
       prop1.max.occ.reached <- as.numeric(names(prop1.max.occ.reached.t) [prop1.max.occ.reached.t])
-      prop2.max.occ.reached.t <- table(data[square[row,1:(column-1)], prop2]) == (sample.size / prop2.nb)
+      prop2.max.occ.reached.t <- table(data[newdata[row,1:(column-1)], prop2]) == (sample.size / prop2.nb)
       prop2.max.occ.reached <- as.numeric(names(prop2.max.occ.reached.t) [prop2.max.occ.reached.t])
 
       not.available <- union(which(data[,prop1] %in% prop1.max.occ.reached), which(data[,prop2] %in% prop2.max.occ.reached));
@@ -166,10 +204,10 @@ fill.counterbalanced.sample.index.matrix <- function(data, prop1, prop2, prop1.n
         } else {
           i <- possible.values
         }
-        square[row,column] <- i;
+        newdata[row,column] <- i;
       remaining <- remaining[-which(remaining == i)];
     }
   }
   cat("\n")
-  return(square)
+  return(newdata)
 }
