ce0d7aa19d6cf46a634a52cf6b797a7d644f5305
Sylvain Loiseau <sylvain.loiseau@univ-paris13.fr>
2020-01-14 16:17:41 +0100

    Cleaning tests

--- tests/testthat/test-counterbalanced.sample.index.matrix.R
+++ tests/testthat/test-counterbalanced.sample.index.matrix.R
@@ -1,43 +1,42 @@
-context("counterbalanced.sample.index.matrix")
+context("balancedSampling")
 
-test_that("counterbalanced.sample.index.matrix: feat1=20, feat2=5, sample.nb=5", {
-  res <- counterbalanced.sample.index.matrix(20, 5, 5)
-
-  frequency.by.row <- apply(res, 1, table)
-  expect_true(all(frequency.by.row == 1))
-
-  frequency.by.column <- apply(res, 2, table)
-  expect_true(all(frequency.by.column == 4))
+test_that("fill.counterbalanced.sample.index.matrix", {
+  data <- data.frame(
+    A=rep(LETTERS[1:4], times=2),
+    B=rep(letters[1:2], each=4)
+  )
+  maxtry <- 50;
+  success <- FALSE;
+  ntry <- 0;
+  while(!success) {
+    res <- fill.counterbalanced.sample.index.matrix(data, c("A", "B"), c(4, 2), c(2, 4), 8, 2, 4);
+    ntry <- ntry + 1
+    if (ntry == maxtry) {
+      break;
+    }
+    success <- res[1]
+  }
 })
 
-test_that("counterbalanced.sample.index.matrix: feat1=5, feat2=20, sample.nb=5", {
-  res <- counterbalanced.sample.index.matrix(5, 20, 5)
+test_that("balancedSamping", {
+  data <- data.frame(
+    A=rep(LETTERS[1:4], times=2),
+    B=rep(letters[1:2], each=4)
+  )
 
-  frequency.by.row <- apply(res, 1, table)
-  expect_true(all(frequency.by.row == 4))
+  res <- balancedSampling(data, vars=c("A", "B"), sample.nb=2)
 
-  frequency.by.column <- apply(res, 2, table)
-  expect_true(all(frequency.by.column == 1))
-})
+  sample1.A <- table(res[[1]][,"A"])
+  expect_true(all(sample1.A == 1))
 
-test_that("counterbalanced.sample.index.matrix: feat1=40, feat2=5, sample.nb=5", {
-  res <- counterbalanced.sample.index.matrix(40, 5, 5)
+  sample1.B <- table(res[[1]][,"B"])
+  expect_true(all(sample1.B == 2))
 
-  frequency.by.row <- apply(res, 1, table)
-  expect_true(all(frequency.by.row == 1))
+  sample2.A <- table(res[[2]][,"A"])
+  expect_true(all(sample2.A == 1))
 
-  frequency.by.column <- apply(res, 2, table)
-  expect_true(all(frequency.by.column == 8))
-})
-
-test_that("counterbalanced.sample.index.matrix: feat1=10, feat2=10, sample.nb=5", {
-  res <- counterbalanced.sample.index.matrix(10, 10, 5)
-
-  frequency.by.row <- apply(res, 1, table)
-  expect_true(all(frequency.by.row == 2))
-
-  frequency.by.column <- apply(res, 2, table)
-  expect_true(all(frequency.by.column == 2))
+  sample2.B <- table(res[[2]][,"B"])
+  expect_true(all(sample2.B == 2))
 })
 
 test_that("counterbalanced.sample.index.matrix: wrong arguments 1", {
--- tests/testthat/test-permute.R
+++ tests/testthat/test-permute.R
@@ -1,6 +1,16 @@
+library(testthat)
 test_that("permute.without.repetition1", {
-  #data.frame(A=LETTERS[1:10], B=sample(rep(1:2, 5)), C=rep(1:5, each=2))
-  df <- data.frame(A=LETTERS[1:10], B=sample(rep(1:2, 5), 10), C=1)
-  print(df)
-  permute.without.repetition(df, "B")
+  data <- data.frame(A=c(rep("a", 3), rep("b", 3)), B=c(rep("c", 3), rep("d", 3)));
+  x <- order.without.repetition(data, c("A", "B"), c(1,1));
+  expect_true(all(table(x$A) == 3))
+  expect_true(all(table(x$B) == 3))
 })
+
+test_that("permute.without.repetition2", {
+  data <- data.frame(A=LETTERS[1:10], B=rep("un", 10), C=1:10)
+  expect_error(
+    order.without.repetition(data, c("A", "B"), c(1,1)),
+    "the maximum number of tries has been reached"
+  );
+})
+
