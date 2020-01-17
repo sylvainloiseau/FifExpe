b95568f2165701ead50d70585d79dd4e157580e2
Sylvain Loiseau <sylvain.loiseau@univ-paris13.fr>
2020-01-17 10:53:31 +0100

    vignette and documentation

---
+++ R/dataset.R
@@ -0,0 +1,10 @@
+#' This is data to be included in my package
+#'
+#' @name fif3con
+#' @docType data
+#' @author Philippe Gréa
+#' @keywords data
+#' @usage data(fif3con)
+#' @format A data frame with 180 rows and 10 variables
+NULL
+
